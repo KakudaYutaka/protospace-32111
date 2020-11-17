f9d2523a10f7869b19b861f774ff2a42cb57618c
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-17 14:10:23 +0900

    コメント実装済み

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :prototype
-  has_many :comment
+  has_many :comments
          
   validates :name, presence: true
   validates :profile, presence: true
